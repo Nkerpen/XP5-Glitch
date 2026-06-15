e21da76f5935db508f4eb92bbf45124c11c4cb7e
JeffRafSantos <jefinho.santos3020@gmail.com>
2026-06-15 20:27:37 -0300

    Update Clock
    Adicionado um relógio no jogo.

---
+++ XP5-Game/Assets/Scripts/GameClockController.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using TMPro;
+
+public class GameClockController : MonoBehaviour
+{
+    [Header("Clock UI")]
+    [SerializeField] private TMP_Text clockText;
+    private float elaspedTime;
+
+    [Header("Time In a Day")]
+    [SerializeField] private float timeInADay = 86400f;
+
+    [Header("Speed Time")]
+    [SerializeField] private float timeScale = 2.0f;
+
+    private void Start()
+    {
+        //elaspedTime = 12 * 3600f;
+    }
+
+    private void Update()
+    {
+        elaspedTime += Time.deltaTime * timeScale;
+        elaspedTime %= timeInADay;
+        UpdateClockUI();
+    }
+
+    void UpdateClockUI()
+    {
+        int hours = Mathf.FloorToInt(elaspedTime / 3600f);
+        int minutes = Mathf.FloorToInt((elaspedTime % 3600f) / 60f);
+        int seconds = Mathf.FloorToInt((elaspedTime % 60f));
+
+        string ampm = hours < 12 ? "AM" : "PM";
+        hours = hours % 12;
+        if (hours == 0)
+        {
+            hours = 12;
+        }
+
+        string clockString = string.Format("{0:00}:{1:00} {2}", hours, minutes, ampm);
+        clockText.text = clockString;
+    }
+}
